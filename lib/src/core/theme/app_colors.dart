52575e2c13aa118d39be71b559952b1a12275c34
Andy <dubinsky.ad@gmail.com>
2025-11-08 13:48:50 +0200

    added AppColors

---
+++ lib/src/core/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color appBar = Color(0xFF171717);
+  static const Color textPrimary = Color(0xFF171717);
+  static const Color primaryButton = Color(0xFF4F46E5);
+  static const Color primaryButtonContent = Color(0xFFFFFFFF);
+  static const Color secondaryButton = Color(0xFFF5F5F5);
+  static const Color secondaryButtonContent = Color(0xFF171717);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color backgroundSecondary = Color(0xFFF3F4F6);
+  static const Color backgroundTertiary = Color(0xFF171717);
+  static const Color icon = Color(0xFFFFFFFF);
+  static const Color loaderBackground = Color(0xFFFF4086);
+  static const Color loaderProgress = Color(0xFF4F46E5);
+  static const Color accent = Color(0xFFF59E0B);
+  static const Color textSpecial = Color(0xFF4F46E5);
+  static const Color dotSpecial = Color(0xFF14B8A6);
+}
